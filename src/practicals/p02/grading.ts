1b1c0ab0257174bd8775156808cae0e910afc0d7
viruskizz <kizzaraiva@gmail.com>
2026-03-20 00:14:05 +0700

    Update grading.ts

--- src/practicals/p02/grading.ts
+++ src/practicals/p02/grading.ts
@@ -11,11 +11,10 @@ if (
   score > 100
 ) {
   console.log("Invalid Input");
-  process.exit(0);
-}
-
-if (score >= 80) console.log("A");
+} else {
+  if (score >= 80) console.log("A");
 else if (score >= 70) console.log("B");
 else if (score >= 60) console.log("C");
 else if (score >= 50) console.log("D");
 else console.log("F");
+}
